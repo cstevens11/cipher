409fe3ef78eb7b3ea4a36b624b2d3f4deb51ee07
cstevens <colestevens24@gmail.com>
2023-02-28 08:26:20 -0800

    final cleanup

--- ruby.rb
+++ ruby.rb
@@ -1,15 +1,11 @@
 def caesar_cipher(string, shift)
-    # caesar_cipher("def", shift)
     input = string.split("").map(&:ord).map(&:to_i)
-    # input = ["d", "e", "f"]
-    # input = [100, 101, 102]
-    # p "input = #{input}"
     newinput = input.map {|number| 
-# needs to exclude all numbers not in letter ord# range
+# excludes all numbers not in letter ord# range
         if (number < 65 || number > 90) && (number < 97 || number > 122) 
             number
         else
-# needs to check number AFTER/IF shifted and subtract 26 if outside of letter ord# range
+# checks number AFTER/IF shifted and subtract 26 if outside of letter ord# range
             if ((number + shift) >= 65 && (number + shift) <= 90) || 
                 ((number + shift) >= 97 && (number + shift) <= 122)
                 number + shift
@@ -18,8 +14,6 @@ def caesar_cipher(string, shift)
             end
         end
     }
-    # p "newinput = #{newinput}"
     finaloutput = newinput.map(&:chr).join("")
-    # p "finaloutput = #{finaloutput}"
     return finaloutput
 end
