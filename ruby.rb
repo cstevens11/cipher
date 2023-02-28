9165fcbd4537138824e310b6afc53dc4a56b4883
cstevens <colestevens24@gmail.com>
2023-02-27 17:00:09 -0800

    convert array to ord #s

--- ruby.rb
+++ ruby.rb
@@ -5,10 +5,14 @@
 # def upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 #     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 def CC(string)
-    lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
-    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    input = string.split("")
+    # CC("apple")
+    input = string.split("").map(&:ord)
+    # input = ["a", "p", "p", "l", "e"]
+    # input = [65, 80, 80, 76, 101]
+
     return input 
 end
+
+
+# space ord# is 32 ---- before shift do each *unless 32* shift
+# for ord # do - after shift, if ord# is not 65 < ord# < 90  OR if 97 < ord# < 122 then ord# - 26
