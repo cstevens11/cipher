1889085dfe889b79c16c21694ebb4b59c3fb2b62
cstevens <colestevens24@gmail.com>
2023-02-27 23:11:12 -0800

    working cipher except for symbols other than space

--- ruby.rb
+++ ruby.rb
@@ -4,13 +4,36 @@
 #     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 # def upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 #     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-def CC(string)
-    # CC("apple")
+def CC(string, shift)
+    # CC("def")
     input = string.split("").map(&:ord)
-    # input = ["a", "p", "p", "l", "e"]
-    # input = [65, 80, 80, 76, 101]
+    # input = ["d", "e", "f"]
+    # input = [100, 101, 102]
+    p "input = #{input}"
+    newinput = input.map {|number| 
+        if number == 32 
+            number
+        else
+            number + shift;
 
-    return input 
+        end
+    }
+    p "newinput = #{newinput}"
+    alphwrap = newinput.map {|number| 
+        if number == 32
+            number
+        elsif (number >= 65 && number <= 90) || (number >= 97 && number <= 122)
+         number
+        else
+            number - 26
+        end
+    }
+    p "alphwrap = #{alphwrap}"
+    finaloutput = alphwrap.map(&:chr).join("")
+    p "finaloutput = #{finaloutput}"
+
+
+
 end
 
 
