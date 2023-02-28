3c4e5442e284f20e4847629ce32deb959e1a2813
cstevens <colestevens24@gmail.com>
2023-02-28 08:18:27 -0800

    clean up finished cipher program

--- ruby.rb
+++ ruby.rb
@@ -1,22 +1,15 @@
-# (arrayIndex + arrayLength) % arrayLength
-
-# def lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-#     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-# def upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
-#     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-def CC(string, shift)
-    # CC("def")
+def caesar_cipher(string, shift)
+    # caesar_cipher("def", shift)
     input = string.split("").map(&:ord).map(&:to_i)
     # input = ["d", "e", "f"]
     # input = [100, 101, 102]
-    p "input = #{input}"
+    # p "input = #{input}"
     newinput = input.map {|number| 
-# needs to exclude all numbers not in letter range
+# needs to exclude all numbers not in letter ord# range
         if (number < 65 || number > 90) && (number < 97 || number > 122) 
             number
         else
-# needs to check number AFTER/IF shifted and subtract 26 if outside of letter range
+# needs to check number AFTER/IF shifted and subtract 26 if outside of letter ord# range
             if ((number + shift) >= 65 && (number + shift) <= 90) || 
                 ((number + shift) >= 97 && (number + shift) <= 122)
                 number + shift
@@ -25,21 +18,8 @@ def CC(string, shift)
             end
         end
     }
-    p "newinput = #{newinput}"
+    # p "newinput = #{newinput}"
     finaloutput = newinput.map(&:chr).join("")
-    p "finaloutput = #{finaloutput}"
-
-
-
+    # p "finaloutput = #{finaloutput}"
+    return finaloutput
 end
-
-# space ord# is 32 ---- before shift do each *unless 32* shift
-# for ord # do - after shift, if ord# is not 65 < ord# < 90  OR if 97 < ord# < 122 then ord# - 26
-
-
-
-
-
-
-
-
